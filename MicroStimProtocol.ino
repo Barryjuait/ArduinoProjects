d4d0686039c848846977335884d93d53c9c34c96
SBarrientos <bio.barryjuaisen@gmail.com>
2022-05-02 15:28:31 +0200

    commit on main

--- MicroStimProtocol.ino
+++ MicroStimProtocol.ino
@@ -1,85 +1,123 @@
 /*
-author: Sebastian Barrientos
-Protocol to send pulses to the stimulator MultiChannel.
+  author: Sebastian Barrientos
+  Protocol to send pulses to the stimulator MultiChannel.
+  HFS and LFS protocol are taken from Picconi et al, 2003.
+
+  Arduino can receive serial messages from C# to trigger the routines.
 
 */
-int pin1 = 7;
-int pin2 = 8;
-int pin3 = 9;
-int pin4 = 10;
-int pin5 = 11;
-int pin6 = 13;
+
+#include <Arduino.h>
+int pins[6] = {7, 8, 9, 10, 11, 13};
+int nPins = sizeof(pins);
+
+char procedure;// Procedure code
+int Freq; // Frequency of the stimulation train
+int Duration; // For how long we stimulate
+char Hemi; // Hemisphere
+int Structure;
+
+int stimElectrode;
+int nPulses;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-    Serial.begin(9600);
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(pin3, OUTPUT);
-  pinMode(pin4, OUTPUT);
-  pinMode(pin5, OUTPUT);
-  pinMode(pin6, OUTPUT);
-  digitalWrite(pin1, LOW);
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3, LOW);
-  digitalWrite(pin4, LOW);
-  digitalWrite(pin5, LOW);
-  digitalWrite(pin6, LOW);
+  Serial.begin(9600);
+  for (int ix = 0; ix < nPins ; ix++) {
+    pinMode(pins[ix], OUTPUT);
   }
+}
 
 void loop() {
   //put your main code here, to run repeatedly:
-  SpeedVal = Serial.parseInt(); //reads only the numbers from the incoming string
-  inByte = Serial.read(); //reads the rest of the characters from the incoming string that are not numbers
-  int valR = 1500 + (SpeedVal*5);
-  int valL = 1500 - (SpeedVal*5);
-  //values of microseconds between 1000 and 1499 will turn the motor left; from 1501 to 2000 will turn it right. 1500 will stop;
-  if (inByte == 'x')
-  {
-    manualMode = false;
-    Serial.println("Manual Mode OFF");
+  while (Serial.read() != '$') ;  // Wait for $
+  procedure = Serial.read();// Procedure code
+  Freq = Serial.parseInt(); // Frequency of the stimulation train
+  Duration = Serial.parseInt(); // For how long we stimulate
+  Hemi = Serial.read(); // Hemisphere
+  Structure = Serial.parseInt();
+
+  //check stim pins:
+  if (Hemi == "l") {
+    if (Structure == 1) {
+      stimElectrode = pins[0];
     }
-  else if (inByte == 'y')
-  {
-    manualMode = true;
-    Serial.println("Manual Mode ON");
+    else if (Structure == 2) {
+      stimElectrode = pins[1];
     }
-    if (press_right == LOW && manualMode == true) {
-    //servoCommutator.attach(servo_pin);
-    Serial.println("right");
-    digitalWrite(LED_control, HIGH);
-    servoCommutator.writeMicroseconds(2000);
+    else if (Structure == 3) {
+      stimElectrode = pins[2];
+    }
+  } else {
+    if (Structure == 1) {
+      stimElectrode = pins[3];
+    }
+    else if (Structure == 2) {
+      stimElectrode = pins[4];
+    }
+    else if (Structure == 3) {
+      stimElectrode = pins[5];
+    }
   }
-  else if (press_left == LOW && manualMode == true) {
-    //servoCommutator.attach(servo_pin);
-    Serial.println("left");
-    digitalWrite(LED_control, HIGH);
-    servoCommutator.writeMicroseconds(1000);
+
+  //Select stim procedure
+  if (procedure == "P") {
+    Probing(Freq, Duration);
   }
-  else if (press_left == HIGH && press_right == HIGH && manualMode == true) {
-    servoCommutator.writeMicroseconds(1500);
-    digitalWrite(LED_control, LOW);
-    Serial.println(" ");
+  else if (procedure == 'H') {
+    HFS(Freq, Duration, stimElectrode);
   }
-  else if (press_left == LOW && press_right == LOW && manualMode == true) {
-    servoCommutator.writeMicroseconds(1500);
-    digitalWrite(LED_control, LOW);
-    Serial.println(" ");
+  else if (procedure == 'L') {
+    LFS(Freq, Duration, stimElectrode);
   }
-  else if (inByte == 'a' && manualMode == false) {
-    servoCommutator.writeMicroseconds(valR);
-    digitalWrite(LED_control, HIGH);
-    Serial.println("Right");
+}
+
+void Probing(int xFreq, int xDuration)
+{
+  nPulses = xDuration/xFreq;
+  float ISI = (1/xFreq)/nPins;
+
+  //Loop for each pin
+  int pinArray[6] = {pins[0],pins[1],pins[2],pins[3],pins[4],pins[5]}; //fix the order depending on the desired series
+  for (int ix = 0; ix <= nPulses; ix++) {
+    digitalWrite(pinArray[ix], 'HIGH');
+    delay(ISI-5);
+    digitalWrite(pinArray[ix], 'LOW');
+    delay(5);
   }
-  else if (inByte == 'b' && manualMode == false) {
-    servoCommutator.writeMicroseconds(valL);
-    digitalWrite(LED_control, HIGH);
-    Serial.println("Left");
-  }  
-  else if (inByte == 'c' && manualMode == false) {
-    servoCommutator.writeMicroseconds(1500);
-    digitalWrite(LED_control, LOW);
-    Serial.println("Stop");
+}
+
+//3 trains, 3sec duration, 100Hz, 20sec interval
+void HFS(int xFreq, int xDuration, int xstimElectrode)
+{
+  int trainDuration = 3; //sec
+  int trainInterval = 20;//sec
+  int rounds = xDuration/trainInterval;
+  nPulses = trainDuration/xFreq;
+  float ISI = (1/xFreq);
+
+  for (int k = 0; k <= rounds; k++){
+    for (int ix = 0; ix <= nPulses; ix++) {
+      digitalWrite(xstimElectrode, 'HIGH');
+      delay(ISI-5);
+      digitalWrite(xstimElectrode, 'LOW');
+      delay(5);
+    }
+    delay(trainInterval);
   }
-                    
+}
+
+//10 min, 2Hz
+void LFS(int xFreq, int xDuration, int xstimElectrode)
+{
+  nPulses = xDuration/xFreq;
+  float ISI = (1/xFreq);
+
+  for (int ix = 0; ix <= nPulses; ix++) {
+    digitalWrite(xstimElectrode, 'HIGH');
+    delay(ISI-5);
+    digitalWrite(xstimElectrode, 'LOW');
+    delay(5);
+  }
+
 }
